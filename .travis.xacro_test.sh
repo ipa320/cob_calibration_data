c623e7d515c2e8bb400233df33b4fa138b276f7c
fmessmer <felixmessmer@gmail.com>
2020-01-15 19:37:14 +0100

    use rosenv for AFTER_SCRIPT

--- .travis.xacro_test.sh
+++ .travis.xacro_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export PYTHONPATH=/opt/ros/$ROS_DISTRO/lib/python2.7/dist-packages
 for x in $(find "$TARGET_REPO_PATH" -name '*.xacro'); do
     echo "Testing $x"
     /opt/ros/$ROS_DISTRO/lib/xacro/xacro --inorder "$x" > /dev/null
