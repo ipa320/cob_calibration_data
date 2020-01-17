3191e37e83936ee72ea713d12fe688198a21e5b6
fmessmer <felixmessmer@gmail.com>
2020-01-17 09:32:11 +0100

    fix .travis.xacro_test

--- .travis.xacro_test.sh
+++ .travis.xacro_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-for x in $(find "$TARGET_REPO_PATH" -name '*.xacro'); do
+status=0
+for x in $(find "$TARGET_REPO_PATH" -name '*.xacro'|sort); do
     echo "Testing $x"
-    /opt/ros/$ROS_DISTRO/lib/xacro/xacro --inorder "$x" > /dev/null
+    xacro --inorder "$x" > /dev/null || status=1
 done
+exit $status
